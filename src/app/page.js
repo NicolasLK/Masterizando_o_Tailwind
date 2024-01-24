e63c9547d4a9b391082791617c412ab6bf56c134
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-01-23 21:01:58 -0300

    feat: Seletores e estados

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,32 @@
 export default function Home() {
   return (
-    <main className="h-screen bg-red-500">
-      <h1 className="font-extrabold">TailWind</h1>
+    <main className="h-screen bg-red-500 p-4">
+      <h1
+        className="font-extrabold 
+      flex 
+      items-center 
+      gap-3 
+      before:w-0.5 before:h-6 before:bg-sky-600 before:flex"
+      >
+        TailWind
+      </h1>
+      <h2 className="text-3xl text-blue-200">Tailwind</h2>
+      <p>Tailwind</p>
+      <button
+        disabled
+        className="bg-sky-500 
+        px-4 
+        py-2 
+        rounded-md 
+        font-semibold 
+        mt-4 
+        cursor-pointer
+        enabled:hover:bg-sky-900 
+        disabled:opacity-60 
+        disabled:cursor-not-allowed"
+      >
+        Sing In
+      </button>
     </main>
   );
 }
