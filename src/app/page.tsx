b2e1e3ce431f182695904e98cdba94248e15788f
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-02-01 20:42:54 -0300

    feat: Criando Abas com Radix Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@
   -> Evitar ao maximo utiliza-los
 */
 
+import { SettingsTabs } from "../components/SettingsTabs";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+    </>
+  );
 }
