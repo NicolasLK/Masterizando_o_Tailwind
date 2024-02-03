ba0fb2b4135232f9773f3dfd6a46deb02c6dea63
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-02-02 21:12:38 -0300

    feat: Criando a Estrutura do Formulário

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
         <div className="grid min-h-screen grid-cols-app">
           <SideBar />
 
-          <main className="px-4 pb-12 pt-8">{children}</main>
+          <main className="px-8 pb-12 pt-8">{children}</main>
         </div>
       </body>
     </html>
