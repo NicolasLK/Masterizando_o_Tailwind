c414bb308e54cd55d5d936057ed0173458bb3cac
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-07-06 17:11:22 -0300

    feat: Input de Biografia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,22 @@
   -> Evitar ao maximo utiliza-los
 */
 
-import { Mail, UploadCloud, User } from "lucide-react";
+import {
+  Bold,
+  Italic,
+  Link,
+  List,
+  ListOrdered,
+  Mail,
+  UploadCloud,
+  User,
+} from "lucide-react";
 import { SettingsTabs } from "../components/SettingsTabs";
 import * as Input from "../components/Input";
 import { FileInput } from "../components/Form/FileInput/index";
 import { Select } from "../components/Form/Select";
 import { SelectItem } from "../components/Form/Select/SelectItem";
+import { TextArea } from "../components/Form/TextArea";
 
 export default function Home() {
   return (
@@ -167,7 +177,57 @@ export default function Home() {
                 Write a short introduction.
               </span>
             </label>
-            <div></div>
+            <div className="space-y-3">
+              <div className="grid gap-3 grid-cols-2">
+                <Select placeholder="" defaultValue="normal">
+                  <SelectItem
+                    value="normal"
+                    defaultChecked
+                    text="Normal Text"
+                  />
+                  <SelectItem value="md" text="Markdown" />
+                </Select>
+                <div className="flex items-center gap-1">
+                  <button
+                    type="button"
+                    className=" p-2 hover:bg-zinc-50 rounded-md"
+                  >
+                    <Bold className="h-4 w-4 text-zinc-500" />
+                  </button>
+                  <button
+                    type="button"
+                    className=" p-2 hover:bg-zinc-50 rounded-md"
+                  >
+                    <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className=" p-2 hover:bg-zinc-50 rounded-md"
+                  >
+                    <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className=" p-2 hover:bg-zinc-50 rounded-md"
+                  >
+                    <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className=" p-2 hover:bg-zinc-50 rounded-md"
+                  >
+                    <ListOrdered
+                      className="h-4 w-4 text-zinc-500"
+                      strokeWidth={3}
+                    />
+                  </button>
+                </div>
+              </div>
+              <TextArea
+                id="bio"
+                defaultValue="I'm a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development."
+              />
+            </div>
           </div>
           <div className="grid gap-3 grid-cols-form pt-5">
             <label
---
+++ src/components/Form/TextArea.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps } from "react";
+
+interface TextAreaProps extends ComponentProps<"textarea"> {}
+
+export function TextArea(props: TextAreaProps) {
+  return (
+    <>
+      <textarea
+        {...props}
+        className="min-h-[7.5rem] resize-y w-full rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      />
+    </>
+  );
+}
