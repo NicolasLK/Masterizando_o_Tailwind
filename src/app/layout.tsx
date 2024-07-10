3d93df38c0dc48cccefee41dd47fe6e70732bf7a
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-07-10 19:42:02 -0300

    feat: Responsividade das abas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="antialiased">
       <body>
-        <div className="grid min-h-screen grid-cols-app">
+        <div className="min-h-screen lg:grid lg:grid-cols-app">
           <SideBar />
 
-          <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">{children}</main>
+          <main className="px-4 pb-12 pt-24 max-w-sc lg:col-start-2 lg:px-8 lg:pt-8">{children}</main>
         </div>
       </body>
     </html>
