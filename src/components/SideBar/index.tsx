40df6a8bc75bfd05719668b1c8eb48f166b3bcb0
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-07-09 21:30:19 -0300

    feat: Abertura da Sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Home,
   BarChart,
@@ -8,48 +9,61 @@ import {
   Users,
   LifeBuoy,
   Cog,
+  Menu,
 } from "lucide-react";
+import * as Collapsible from "@radix-ui/react-collapsible"
 import { Logo } from "./Logo";
 import { NavItem } from "./NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
 import * as Input from "../Input";
+import { Button } from "../Button";
 
 export function SideBar() {
   return (
     <>
-      <aside className="flex flex-col gap-6 border-b border-zinc-200 fixed left-0 top-0 right-0 bottom-0 z-20 bg-white p-4 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative">
-        <Logo />
+      <Collapsible.Root className="flex flex-col gap-6 border-b border-zinc-200 fixed left-0 top-0 right-0 data-[state=open]:bottom-0 data-[state=open]:h-screen z-20 bg-white p-4 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 lg:data-[state=closed]:h-screen">
+        <div className="flex items-center justify-between">
+          <Logo />
+          <Collapsible.Trigger asChild className="lg:hidden">
+            <Button variant="ghost">
+              <Menu className="h-6 w-6" />
+            </Button>
+          </Collapsible.Trigger>
+        </div>
 
-        <Input.Root>
-          <Input.Prefix>
-            <Search className="h-5 w-5 text-zinc-500" />
-          </Input.Prefix>
-          <Input.Control placeholder="Search" />
-        </Input.Root>
 
-        <nav className="space-y-0.5">
-          <NavItem title="Home" icon={Home} />
-          <NavItem title="Dashboard" icon={BarChart} />
-          <NavItem title="Projects" icon={SquareStack} />
-          <NavItem title="Tasks" icon={CheckSquare} />
-          <NavItem title="Reporting" icon={Flag} />
-          <NavItem title="Users" icon={Users} />
-        </nav>
+        <Collapsible.Content forceMount className="flex flex-col flex-1 gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex">
+          <Input.Root>
+            <Input.Prefix>
+              <Search className="h-5 w-5 text-zinc-500" />
+            </Input.Prefix>
+            <Input.Control placeholder="Search" />
+          </Input.Root>
 
-        <section className="mt-auto flex flex-col gap-6">
           <nav className="space-y-0.5">
-            <NavItem title="Support" icon={LifeBuoy} />
-            <NavItem title="Settings" icon={Cog} />
+            <NavItem title="Home" icon={Home} />
+            <NavItem title="Dashboard" icon={BarChart} />
+            <NavItem title="Projects" icon={SquareStack} />
+            <NavItem title="Tasks" icon={CheckSquare} />
+            <NavItem title="Reporting" icon={Flag} />
+            <NavItem title="Users" icon={Users} />
           </nav>
-        </section>
 
-        <UsedSpaceWidget />
+          <section className="mt-auto flex flex-col gap-6">
+            <nav className="space-y-0.5">
+              <NavItem title="Support" icon={LifeBuoy} />
+              <NavItem title="Settings" icon={Cog} />
+            </nav>
+          </section>
 
-        <hr />
+          <UsedSpaceWidget />
 
-        <Profile />
-      </aside>
+          <hr />
+
+          <Profile />
+        </Collapsible.Content>
+      </Collapsible.Root>
     </>
   );
 }
