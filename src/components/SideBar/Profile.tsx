8119298283a6f4f3792e8805f67ff147cb5690cb
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-07-13 15:54:54 -0300

    feat: Theme Switcher AND Change some configs

--- src/components/SideBar/Profile.tsx
+++ src/components/SideBar/Profile.tsx
@@ -3,24 +3,26 @@ import { Button } from "../Button";
 
 export function Profile() {
   return (
-    <section className="grid items-center gap-3 grid-cols-profile">
-      <img
-        src="https://github.com/NicolasLK"
-        className="h-10 w-10 rounded-full"
-        alt=""
-      />
+    <>
+      <section className="flex items-center gap-3">
+        <img
+          src="https://github.com/NicolasLK"
+          className="h-10 w-10 rounded-full"
+          alt=""
+        />
 
-      <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
-          Nicolas Loffi Kaminski
-        </span>
-        <span className="text-sm text-zinc-500 truncate dark:text-zinc-400">
-          nicolas.kaminski.2001@gmail.com
-        </span>
-      </div>
-      <Button type="button" variant="ghost">
-        <LogOut className="h-5 w-5 text-zinc-500" />
-      </Button>
-    </section>
+        <div className="flex flex-1 flex-col overflow-hidden">
+          <span className="text-sm font-semibold text-zinc-700 truncate dark:text-zinc-100">
+            Nicolas Loffi Kaminski
+          </span>
+          <span className="text-sm text-zinc-500 truncate dark:text-zinc-400">
+            nicolas.kaminski.2001@gmail.com
+          </span>
+        </div>
+        <Button type="button" variant="ghost">
+          <LogOut className="h-5 w-5 text-zinc-500" />
+        </Button>
+      </section>
+    </>
   );
 }
